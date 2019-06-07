3fb0393dcf5cbdf5894c3d92ac5091a012619cec
Szemann Peter <pete@rollet.hu>
2019-06-07 17:01:30 -0400

    functionality change - input is taken on click, minor refactor

--- app/src/main/java/peet/learn/clickcounterapp/MainActivity.kt
+++ app/src/main/java/peet/learn/clickcounterapp/MainActivity.kt
@@ -1,40 +1,32 @@
 package peet.learn.clickcounterapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.text.method.ScrollingMovementMethod
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private var userInput: EditText? = null
-    private var button: Button? = null
     private var textView: TextView? = null
-    private var clicksAmount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userInput = findViewById<EditText>(R.id.editText)
-        button = findViewById<Button>(R.id.button)
-        textView = findViewById<TextView>(R.id.textView)
+        val userInput: EditText = findViewById(R.id.editText)
+        val button: Button = findViewById(R.id.button)
+        textView = findViewById(R.id.textView)
 
         textView?.text = ""
-
         textView?.movementMethod = ScrollingMovementMethod()
 
-        button?.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(p0: View?) {
-                clicksAmount += 1
-                if (clicksAmount == 1) {
-                    textView?.append("The button got clicked $clicksAmount time\n")
-                } else {
-                    textView?.append("The button got clicked $clicksAmount times\n")
-                }
-            }
-        })
+        userInput.setText("")
+
+        button.setOnClickListener {
+            textView?.append(userInput.text)
+            textView?.append("\n")
+            userInput.setText("")
+        }
     }
 }
