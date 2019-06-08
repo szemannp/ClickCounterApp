33a7282719a0d3e8a52e40455bc9d297af7ad011
Szemann Peter <pete@rollet.hu>
2019-06-08 15:54:15 -0400

    minor refactor, experiments with logging and lifecycle

--- app/src/main/java/peet/learn/clickcounterapp/MainActivity.kt
+++ app/src/main/java/peet/learn/clickcounterapp/MainActivity.kt
@@ -3,14 +3,19 @@ package peet.learn.clickcounterapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+
+private const val TAG = "MainActivity"
+private const val TEXT_CONTENTS  = "TextContent"
 
 class MainActivity : AppCompatActivity() {
     private var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -22,11 +27,50 @@ class MainActivity : AppCompatActivity() {
         textView?.movementMethod = ScrollingMovementMethod()
 
         userInput.setText("")
+        userInput.hint = "Type something"
 
         button.setOnClickListener {
+            Log.d(TAG, "onClick called")
             textView?.append(userInput.text)
             textView?.append("\n")
             userInput.setText("")
         }
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart called")
+        super.onStart()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onRestoreInstance called")
+        super.onRestoreInstanceState(savedInstanceState)
+        textView?.text = savedInstanceState?.getString(TEXT_CONTENTS, "")
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume called")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause called")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        Log.d(TAG, "onSaveInstance called")
+        super.onSaveInstanceState(outState)
+        outState?.putString(TEXT_CONTENTS, textView?.text.toString())
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "onRestart called")
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy called")
+        super.onDestroy()
     }
 }
